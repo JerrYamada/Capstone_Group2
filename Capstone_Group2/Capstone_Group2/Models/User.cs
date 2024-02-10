ac969db19aa13b63325db9964c9637ba93e32ea4
Romnick <Rzinampan4187@conestogac.on.ca>
2024-02-10 12:09:27 -0500

    Add Constructors on User model

--- Capstone_Group2/Capstone_Group2/Models/User.cs
+++ Capstone_Group2/Capstone_Group2/Models/User.cs
@@ -1,6 +1,25 @@
-﻿namespace Capstone_Group2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Capstone_Group2.Models
 {
     public class User
     {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Password and confirmation password do not match.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
     }
 }
