6e3e23413a17bca7bb7fd11a5ca67b3786fcb090
Romnick <Rzinampan4187@conestogac.on.ca>
2024-02-15 11:26:38 -0500

    Added my part, we will add the Authentication & Authorization part after we create the Data bases

--- Capstone_Group2/Capstone_Group2/Program.cs
+++ Capstone_Group2/Capstone_Group2/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=RegistrationLogin}/{action=Register}");
+    pattern: "{controller=Account}/{action=Login}");
 
 app.Run();
