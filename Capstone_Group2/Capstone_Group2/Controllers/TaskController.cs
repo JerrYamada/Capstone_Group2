0917a6f822b6c3d85788b430f98fd5773721267d
James Arand <jarand7849@conestogac.on.ca>
2024-02-16 15:22:26 -0500

    Added TaskController, TaskViewModel, Task View

---
+++ Capstone_Group2/Capstone_Group2/Controllers/TaskController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Capstone_Group2.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult GetAllTasks()
+        {
+            return View("Tasks");
+        }
+    }
+}
